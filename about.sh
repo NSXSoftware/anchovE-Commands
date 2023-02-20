f0d80708261e7d5df35a4f6bdf1c6d892c4a0aed
NSXSoftware <125416880+NSXSoftware@users.noreply.github.com>
2023-02-20 16:29:07 -0500

    Update about.sh

--- about.sh
+++ about.sh
@@ -1,5 +1,5 @@
 echo -n "[Hostname: $(hostname) ]"
-echo -n "[Operating] System: $(uname -s) ]"
+echo -n "[Operating System: $(uname -s) ]"
 echo -n "[Kernel Version: $(uname -r) ]"
 echo -n "[Processor Type: $(uname -p) ]"
 echo -n "[Available Memory: $(free -m | awk 'NR==2{print $4}') MB ]"
