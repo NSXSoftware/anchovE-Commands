acff21ea379ff73569a60d8303230424052cc982
NSXSoftware <125416880+NSXSoftware@users.noreply.github.com>
2023-02-20 17:02:06 -0500

    Update about.bat

--- about.bat
+++ about.bat
@@ -7,6 +7,6 @@ for /f "tokens=1,2" %%a in ('wmic os get TotalVisibleMemorySize /value') do if "
 set "memory=%memory:~0,-3%"
 for /f "tokens=2 delims==" %%a in ('wmic logicaldisk where deviceid="C:" get size /value') do set "disk_size=%%a"
 for /f "tokens=2 delims==" %%a in ('wmic logicaldisk where deviceid="C:" get freespace /value') do set "free_space=%%a"
-set "disk_size=%disk_size:~0,-9%"
-set "free_space=%free_space:~0,-9%"
+set "disk_size=%disk_size%"
+set "free_space=%free_space%"
 echo [Hostname: %hostname%] [Operating System: %os%] [Kernel Version: %kernel_version%] [Processor Type: %processor_type%] [Available Memory: %memory% MB] [Available Disk Space: %free_space%/%disk_size%]
